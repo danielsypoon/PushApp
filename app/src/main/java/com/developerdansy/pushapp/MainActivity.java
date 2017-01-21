05973ca2b3e2f095bb3ef16013980e81b845ef65
danielsypoon <danielsypoon@gmail.com>
2017-01-21 18:40:24 +0800

    branch 02 do something

--- app/src/main/java/com/developerdansy/pushapp/MainActivity.java
+++ app/src/main/java/com/developerdansy/pushapp/MainActivity.java
@@ -9,10 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO delete something
     }
-
-    // TODO implement checkout new branch
-
-    // TODO implement branch02 and do pull request
 
 }
